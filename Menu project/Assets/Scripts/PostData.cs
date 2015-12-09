65876a60ab7d2d8cf1861978c08d0356bd75c7f2
Dylan231 <dylanvanopdurp@gmail.com>
2015-12-09 12:15:26 +0100

    Updated Menu
    Working on Ui animations

--- Menu project/Assets/Scripts/PostData.cs
+++ Menu project/Assets/Scripts/PostData.cs
@@ -9,31 +9,28 @@ using System;
 
 //namespace Server
 //{
-    public class PostData
+    public class PostData: MonoBehaviour
     {
 
        
-   public void send()
+   public static void send()
         //static void Main(string[] args, int a)
         {
             
             String name = Id.name;
-            String a1 = "aantal schapen dood";
             String a2 = link.a.ToString();
-
+            int y = 9;
+            String a3 = y.ToString();
             Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             sck.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 23));
             sck.Listen(0);
 
             Socket acc = sck.Accept();
-
-            byte[] buffer = Encoding.Default.GetBytes(name);      
+        // all the data will  be sent in one buffer.
+            byte[] buffer = Encoding.Default.GetBytes(name + a2);      
            
-            byte[] buffer1 = Encoding.Default.GetBytes(a1 + name);
             acc.Send(buffer, 0, buffer.Length, 0);
-            acc.Send(buffer1, 0, buffer.Length, 0);
-
             buffer = new byte[255];
             int rec = acc.Receive(buffer, 0, buffer.Length, 0);
             Array.Resize(ref buffer, rec);
---
+++ Menu project/Assets/Scripts/PostData2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Collections;
+
+public class PostData2 : MonoBehaviour {
+
+    public void sendData() {
+        String name = Id.name;
+        String a1 = "aantal schapen dood";
+        int a2 = link.a;
+        int y = 1;
+
+        string url = "https://drproject.twi.tudelft.nl/ewi3620tu6/";
+
+        WWWForm form = new WWWForm();
+        // adding an int to a form
+        form.AddField("score", a2);
+        //adding a String to a form
+        form.AddField(name, 1);
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        //check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+
+        }
+    }
+
+	
