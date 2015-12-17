31fdcb1c71f4a8f57928794a9c8d3ee002b274b4
Dylan231 <dylanvanopdurp@gmail.com>
2015-12-17 16:23:50 +0100

    Updated Meun
    With id check

--- Menu project/Assets/Scripts/Id.cs
+++ Menu project/Assets/Scripts/Id.cs
@@ -3,6 +3,77 @@ using System.Collections;
 
 public class Id : MonoBehaviour
 {
+    public Canvas canvas1;
+    public Canvas canvas2;
+    public GameObject t1;
+    string url = "https://drproject.twi.tudelft.nl/ewi3620tu6/checkusername.php";
+    public WWW www;
+
+    public void checkname()
+    {
+
+
+        StartCoroutine(WaitForRequest());
+
+    }
+
+    public IEnumerator WaitForRequest()
+
+    {
+
+        //  yield return new WaitForEndOfFrame();
+        WWWForm form = new WWWForm();
+        form.AddField("username", Id.name);
+        www = new WWW(url, form);
+        yield return www;
+        t1.gameObject.SetActive(false);
+
+
+
+
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            print(www.error);
+        }
+        else
+        {
+
+            print("Finished checking name");
+        }
+        //waitname();
+
+            if (www.text.Trim().Equals("1"))
+
+            {
+
+                canvas1.gameObject.SetActive(true);
+                canvas2.gameObject.SetActive(false);
+
+            }
+            else
+            {
+                //Debug.Log(www.text);
+                Debug.Log("wrong name");
+            t1.gameObject.SetActive(true);
+            }
+        }
+
+
+
+    public void waitname()
+    {
+        StartCoroutine(wait());
+    }
+
+        IEnumerator wait()
+    {
+            Debug.Log("Before Waiting 2 seconds");
+            yield return new WaitForSeconds(5);
+            Debug.Log("After Waiting 2 Seconds");
+        }
+
+    
 
     public static string name = "LOL";
 
--- Menu project/Assets/Scripts/Main_Menu.cs
+++ Menu project/Assets/Scripts/Main_Menu.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class Main_Menu : MonoBehaviour {
-   
+  
+
+    private static text way;
+    private int i;
+    string url = "https://drproject.twi.tudelft.nl/ewi3620tu6/checkusername.php";
+    public WWW www;
+
     public void Quitgame()
     {
         Debug.Log("Game is exiting...");
